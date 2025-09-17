6bed76e4188a057a56fadce03431a3eab7d5853b
torilima <abelyvia.tori@ui.ac.id>
2025-09-17 15:55:39 +0700

    Tutorial3

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class News(models.Model):
@@ -12,7 +12,7 @@ class News(models.Model):
         ('rumor', 'Rumor'),
         ('analysis', 'Analysis'),
     ]
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.TextField()
