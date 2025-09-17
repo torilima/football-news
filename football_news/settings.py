8a592c1786d64c98e9618224fdb8eea2e3968bfe
torilima <abelyvia.tori@ui.ac.id>
2025-09-17 16:17:45 +0700

    tutorialll 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "abelyvia-tori-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/abelyvia.tori/footballnews"
+    "https://abelyvia-tori-footballnews.pbp.cs.ui.ac.id"
 ]
 # Application definition
 
